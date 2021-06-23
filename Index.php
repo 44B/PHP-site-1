dc2cdb1c993ed20508474e1d8ac3c10cd3b2b530
mohur <m.ashrahfi@gmail.com>
2021-06-22 17:51:26 -0700

    Add conditionals whenever invalid/short passwords are submitted

--- Index.php
+++ Index.php
@@ -1,10 +1,39 @@
-<?php
+<h2>You have entered: <?php
+                        echo $checkPassword;
+                        ?>
+</h2>
+<h3><?php
+    function read_text($text)
+    {
+        $invalid_passwords = array("12345678", "password", "picture1", "iloveyou", "qqww1122", "unknowns", "sunshine", "aaaaaaaa", "55555555", "superman", "samantha");
+        //CARRIES GLOBAL VARIABLE TO THE FUNCTION
+        global $error;
+        if (strlen($text) < 8) {
+            $error = "1";
+            echo "<script>alert('Password must be 8 characters or more.');</script>";
+        }
 
-$greeting = 'Hello World';
+        foreach ($invalid_passwords as $item) {
+            //lowercase the entered password so there are no variations from array item.
+            $lowerCasePassword = strtolower($text);
+            if ($item === $lowerCasePassword) {
+                $error = "1";
+                echo "<script>alert('Password not allowed as it has been blacklisted.');</script>";
+            }
+        }
+    }
 
-function read_text($text)
-{
-    $text = strtolower($text);
-}
+    function is_error($error)
+    {
+        global $error;
+        if ($error === "1") {
+            echo "Password check failed. Please enter another password.";
+        } elseif ($error === "0") {
+            echo "<h1>Password check passed!</h1>";
+        }
+    }
 
-require 'index.view.php';
+
+    echo read_text($checkPassword);
+    is_error($error);
+    ?></h3>
--- index.view.php
+++ index.view.php
@@ -5,21 +5,29 @@
 
 <head>
     <title> Check Password Strength </title>
-    <!-- <link rel=stylesheet href=index.css> -->
-
+    <link rel=stylesheet href=index.css>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Press+Start+2P&display=swap" rel="stylesheet">
 </head>
 
-<body>
+<body id="body">
 
     <h1>Active Password Checker</h1>
     <div id="parentContainer">
         <form action="index.view.php" method="get">
             <h3>Password:</h3>
-            <input type="text" name="checkPassword" id="passwordValue">
+            <input type="password" name="password">
             <input type="submit"></input>
         </form>
-
     </div>
 </body>
+
+<?php
+$checkPassword =  $_GET["password"];
+$error = "0";
+include "index.php";
+?>
+
 
 </html>
